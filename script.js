3766e5a32e08f702f9040cdce6d00ec4f32c67bc
Lucas Ximenes <82716172+ximenesl@users.noreply.github.com>
2025-08-24 17:05:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-document.getElementById("btn-print").addEventListener(
-    "click", () => {
-        window.print();
-    }
-
-)
+// Busca no documento o elemento com id "btn-print"
+document.getElementById("btn-print")
+  // Adiciona um evento para quando o botão for clicado
+  .addEventListener(
+    "click",               // Tipo de evento que queremos: "click" (clique do mouse)
+    () => {                // Função anônima (arrow function) que será executada quando o botão for clicado
+        window.print();    // Comando que abre a caixa de diálogo de impressão do navegador
+    }                      // Fecha a função
+  );                       // Fecha o addEventListener
